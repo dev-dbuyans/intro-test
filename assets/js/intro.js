d72eccab3b8a418a76bb553fd09bbf092efa8fc8
Bynzii <bynzi92@gmail.com>
2025-07-17 11:35:58 +0900

    test

--- assets/js/intro.js
+++ assets/js/intro.js
@@ -1,31 +1,59 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  //visual hover
-  $("section#intro .con").hover(
+  // 📱 터치 디바이스인지 판별
+  function isTouchDevice() {
+    return ('ontouchstart' in window) || navigator.maxTouchPoints > 0;
+  }
 
-    function(){
-      $(this).addClass('on')
-      $(this).find('.effect').addClass('on')
+  // PC: hover 이벤트
+  $("section#intro .con").hover(
+    function () {
+      $(this).addClass('on');
+      $(this).find('.effect').addClass('on');
       $("section#intro .con").not(this).addClass('off');
     },
-    function(){
-      $(this).removeClass('on')
-      $(this).find('.effect').removeClass('on')
+    function () {
+      $(this).removeClass('on');
+      $(this).find('.effect').removeClass('on');
       $("section#intro .con").not(this).removeClass('off');
-
     }
   );
 
-
-  //bg 전환
-  $("section#intro .con").on("mouseover", function () {
-    const classList = $(this).attr("class").split(" ");
+  // 배경 전환 
+  function setBackground($el) {
+    const classList = $el.attr("class").split(" ");
     const riverCode = classList.find(c => ["HG", "NDG", "GG", "YSG"].includes(c));
-
     if (riverCode) {
       $("#intro_wrap").css({
         backgroundImage: `url('./assets/img/intro_bg_${riverCode}.jpg')`
       });
-    } 
+    }
+  }
+
+  // 모바일 전용: click 이벤트 추가
+  if (isTouchDevice()) {
+    $("section#intro .con").on("click", function () {
+      const $this = $(this);
+      const isActive = $this.hasClass("on");
+
+      $("section#intro .con").removeClass("on off");
+      $("section#intro .con .effect").removeClass("on");
+
+      if (!isActive) {
+        $this.addClass("on");
+        $this.find(".effect").addClass("on");
+        $("section#intro .con").not($this).addClass("off");
+
+        setBackground($this); // 배경 전환도 함께 처리
+      }
+    });
+  }
+
+  // 🖱️ PC용 배경 전환 유지
+  $("section#intro .con").on("mouseover", function () {
+    if (!isTouchDevice()) {
+      setBackground($(this));
+    }
   });
+
 });
