c1da371d40a08be7ea0d5f75bb2dd777a8bf9471
Bynzii <bynzi92@gmail.com>
2025-07-17 12:10:04 +0900

    test

--- assets/js/intro.js
+++ assets/js/intro.js
@@ -1,36 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-  // 터치 디바이스인지 판별
+  // 터치 디바이스 판별
   function isTouchDevice() {
     return ('ontouchstart' in window) || navigator.maxTouchPoints > 0;
   }
 
-  // PC: hover 이벤트
-  $("section#intro .con").hover(
-    function () {
-      $(this).addClass('on');
-      $(this).find('.effect').addClass('on');
-      $("section#intro .con").not(this).addClass('off');
-    },
-    function () {
-      $(this).removeClass('on');
-      $(this).find('.effect').removeClass('on');
-      $("section#intro .con").not(this).removeClass('off');
-    }
-  );
+  // PC: hover 이벤트만 동작
+  if (!isTouchDevice()) {
+    $("section#intro .con").hover(
+      function () {
+        $(this).addClass('on');
+        $(this).find('.effect').addClass('on');
+        $("section#intro .con").not(this).addClass('off');
+      },
+      function () {
+        $(this).removeClass('on');
+        $(this).find('.effect').removeClass('on');
+        $("section#intro .con").not(this).removeClass('off');
+      }
+    );
+  }
 
-  // 배경 전환 
-  function setBackground($el) {
-    const classList = $el.attr("class").split(" ");
+  // 배경 전환
+  $("section#intro .con").on("mouseover", function () {
+    const classList = $(this).attr("class").split(" ");
     const riverCode = classList.find(c => ["HG", "NDG", "GG", "YSG"].includes(c));
     if (riverCode) {
       $("#intro_wrap").css({
         backgroundImage: `url('./assets/img/intro_bg_${riverCode}.jpg')`
       });
     }
-  }
+  });
 
-  // 모바일 전용: click 이벤트 추가
+  // 모바일 전용: click으로 on/off 클래스 처리
   if (isTouchDevice()) {
     $("section#intro .con").on("click", function () {
       const $this = $(this);
@@ -43,17 +45,10 @@ document.addEventListener("DOMContentLoaded", () => {
         $this.addClass("on");
         $this.find(".effect").addClass("on");
         $("section#intro .con").not($this).addClass("off");
-
-        setBackground($this); // 배경 전환도 함께 처리
+        // 배경 전환은 mouseover에 이미 연결돼 있으므로 그대로 유지됨
       }
     });
   }
 
-  // PC용 배경 전환 유지
-  $("section#intro .con").on("mouseover", function () {
-    if (!isTouchDevice()) {
-      setBackground($(this));
-    }
-  });
-
 });
+
